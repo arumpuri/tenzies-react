f6b2e921f8a702c9ee50f2da6c7a628f36b6c1b6
Arum Puri <arumpuripratamawati@gmail.com>
2024-02-10 11:02:43 +0700

    adding roll button and its function

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import Die from './die'
+import {nanoid} from 'nanoid'
 
 function App() {
 
@@ -9,18 +10,26 @@ function App() {
   function allNewDice() {
     const newDice = []
     for (let i= 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6))
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      })
     }
     return newDice
   }
 
-  const diceElements = dice.map(die => <Die value={die} />)
+  function rollDice() {
+    setDice(allNewDice())
+  }
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} />)
 
   return (
     <main>
       <div className='dice-container'>
        {diceElements}
       </div>
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
     </main>
   )
 }
